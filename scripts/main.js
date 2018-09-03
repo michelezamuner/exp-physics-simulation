6b01bf47bb678bc149a51ae2921c157cd298e47a
Michele Zamuner <michelezamuner@gmail.com>
2018-09-03 07:32:00 +0200

    Added some libraries

---
+++ scripts/main.js
@@ -0,0 +1,3 @@
+import {Vec} from '../lib/geometry/geometry.js';
+
+console.log(new Vec(1, 1));
---
+++ scripts/tests.js
@@ -0,0 +1,3 @@
+import * as GeometryTest from '../lib/geometry/test.js';
+import * as PhysicsTest from '../lib/physics/test.js';
+import * as PhysicsSimulationTest from '../lib/physics-simulation/test.js';
