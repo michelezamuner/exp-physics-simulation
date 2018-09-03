e6ea2f285a9bd522db322dbbc38551738aa8bee2
Michele Zamuner <michelezamuner@gmail.com>
2018-09-03 12:48:21 +0200

    Improved tests structure

--- scripts/tests.js
+++ scripts/tests.js
@@ -1,3 +1,16 @@
 import * as GeometryTest from '../lib/geometry/test.js';
 import * as PhysicsTest from '../lib/physics/test.js';
 import * as PhysicsSimulationTest from '../lib/physics-simulation/test.js';
+
+function test(suite) {
+    Object.keys(suite).forEach(testCase => {
+        Object.keys(suite[testCase]).forEach(test => {
+            console.log('Test: ' + test);
+            suite[testCase][test]();
+        })
+    })
+}
+
+test(GeometryTest);
+test(PhysicsTest);
+test(PhysicsSimulationTest);
